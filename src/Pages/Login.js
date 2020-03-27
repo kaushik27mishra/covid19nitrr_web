6a8b26dd560ae6498610de653204ab12ae0109f5
Kaushik Mishra <kmishra27.km@gmail.com>
2020-03-27 19:50:53 +0530

    state of login added

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useUserDispatch, loginUser } from "../Context/UserContext";
 
-export class Login extends Component {
-    render() {
-        return (
-            <div>
-            </div>
-        )
-    }
+function Login(props) {
+    var userDispatch = useUserDispatch();
+    var [isLoading, setIsLoading] = useState(false);
+    var [error, setError] = useState(null);
+    var [activeTabId, setActiveTabId] = useState(0);
+    var [nameValue, setNameValue] = useState("");
+    var [loginValue, setLoginValue] = useState("");
+    var [passwordValue, setPasswordValue] = useState("");
+    return (
+        <div>
+            
+        </div>
+    )
 }
 
 export default Login
+
 
 // onClick={() =>
 //     loginUser(
