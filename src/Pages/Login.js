7a2395d1ace3e50f014018a79ee376ad7b415dda
Kaushik Mishra <kmishra27.km@gmail.com>
2020-03-28 18:16:39 +0530

    login page using bootstrap 4

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -10,36 +10,36 @@ function Login(props) {
     var [loginValue, setLoginValue] = useState("");
     var [passwordValue, setPasswordValue] = useState("");
     return (
-        <div>
-            <h1>Login page</h1>
-            <h6>Email</h6>
-            <input type="text" onChange={(e) => {setLoginValue(e.target.value)}} value={loginValue}></input>
-            <h6>Password</h6>
-            <input type="text" onChange={(e) => {setPasswordValue(e.target.value)}} value={passwordValue}></input>
-            <button onClick={() =>
-    loginUser(
-      userDispatch,
-      loginValue,
-      passwordValue,
-      props.history,
-      setIsLoading,
-      setError,
-    )
-  }>Login</button>
+      <div className="container" style={{paddingTop: "100px",maxWidth:"400px",display:"flex",flexDirection:"row"}}>
+        
+        <br/>
+        <br/>
+        <br/>
+        <div class="card">
+            <div class="card-header">
+              MI19 Admin Panel
+            </div>
+          <div class="card-body">
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" value={loginValue} onChange={(e) => setLoginValue(e.target.value)} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                {/* <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> */}
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" value={passwordValue} onChange={(e) => setPasswordValue(e.target.value)} class="form-control" id="exampleInputPassword1"/>
+              </div>
+              <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+              </div>
+              <button type="submit" onClick={() => loginUser(userDispatch,loginValue,passwordValue,props.history,setIsLoading,setError,)} class="btn btn-primary">Submit</button>
+            </form>
+          </div>
         </div>
+      </div>
     )
 }
 
-export default Login
-
-
-// onClick={() =>
-//     loginUser(
-//       userDispatch,
-//       loginValue,
-//       passwordValue,
-//       props.history,
-//       setIsLoading,
-//       setError,
-//     )
-//   }
+export default Login;
