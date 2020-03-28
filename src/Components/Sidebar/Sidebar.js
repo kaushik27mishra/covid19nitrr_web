9559630db4a03a890247efe11a39c48035ab4fe4
Aryan Sarkar <aryansarkar13@users.noreply.github.com>
2020-03-28 18:33:34 +0530

    Sidebar UI improved

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -7,20 +7,20 @@ import { Link } from 'react-router-dom';
 export default function Sidebar() {
  
   return (
-    <div className="wrapper" >
-    <nav id="sidebar">
+    <div className="wrapper" style={{border:"1px solid grey", boxShadow:"1px 1px 10px grey"}}>
+    <nav id="sidebar" style={{background:"white"}}>
 
         <ul className="list-unstyled components">
             {/* <p>Dummy Heading</p> */}
-            <li>
-            <FontAwesomeIcon icon="home"/>
-            <Link to='/app/dashboard'>Dashboard</Link>
+            <li style={{marginLeft:"5vw"}}>
+            {/*<FontAwesomeIcon icon="home"/>*/}
+            <Link to='/app/dashboard' style={{color:"grey"}}>Dashboard</Link>
             </li>
-            <li>
-                <Link to='/app/register'>Add User</Link>
+            <li style={{marginLeft:"5vw"}}>
+                <Link to='/app/register' style={{color:"grey"}}>Add User</Link>
             </li>
-            <li>
-            <Link to='/app/profiles'>Profiles</Link>
+            <li style={{marginLeft:"5vw"}}>
+            <Link to='/app/profiles' style={{color:"grey"}}>Profiles</Link>
             </li>
         </ul>
 
